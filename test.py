0c58c472d94f76d5f20b1259e57f7adf50eff36e
samyakbvs <34275800+samyakbvs@users.noreply.github.com>
2018-08-02 23:50:31 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import resultsEvaluation as re
-
-hr = ["blue", "green", "red", "yellow"]
-finalPoints = [12, 4, 7, 9] # r, b, g, y
-
-# print(re.posPoints, re.houses)
-finalPoints = re.addResults(hr, finalPoints)
-print(finalPoints)
